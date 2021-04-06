a03bdaee0b69a508fc332228866b99c5ce89a505
Sevos83 <81201994+Sevos83@users.noreply.github.com>
2021-04-06 14:34:54 +0400

    Add files via upload

---
+++ train.rb
@@ -0,0 +1,63 @@
+class Train
+  attr_reader :number, :type, :route, :current_station
+
+  def initialize(number, type, carriages)
+    @number = number
+    @type = type
+    @carriages = carriages
+    @speed = 0
+    register_instance
+  end
+
+  def speed_up(speed)
+    @speed += speed
+  end
+
+  def speed_down(speed)
+    @speed -= speed
+    @speed = @speed > 0 ? @speed : stop
+  end
+
+  def stop
+    @speed = 0
+  end
+
+  def add_carriages
+    @carriages += 1 if @speed == 0
+  end
+
+  def delete_carriages
+    @carriages -= 1 if @speed == 0 && carriages > 0
+  end
+
+  def route=(route)
+    @route = route
+    @current_station = @route.stations.first
+    @current_station_index = @route.stations.index(@current_station)
+    @current_station.train_in(self)
+  end
+
+  def next_station
+    @route.stations[@current_station_index + 1] if @route.stations[@current_station_index] != @route.stations.last
+  end
+
+  def move_next_station
+    return unless next_station
+    @current_station.train_out(self)
+    next_station.train_in(self)
+    @current_station_index += 1
+    @current_station = @route.stations[@current_station_index]
+  end
+
+  def previous_station
+     @route.stations[@current_station_index - 1] if @route.stations[@current_station_index] != @route.stations.first
+  end
+
+  def move_previous_station
+    return unless previous_station
+    @current_station.train_out(self)
+    previous_station.train_in(self)
+    @current_station_index -= 1
+    @current_station = @route.stations[@current_station_index]
+  end
+end
